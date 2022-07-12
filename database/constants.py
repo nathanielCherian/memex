2602ff04573a37f6b40284ad27dfc67960e0f196
Nathaniel Cherian <ngcherian@gmail.com>
2022-07-12 13:07:00 -0400

    setup database

---
+++ database/constants.py
@@ -0,0 +1,2 @@
+
+DB_PATH = "example.db"
--- database/main.py
+++ database/main.py
@@ -1,7 +1,28 @@
+from asyncio import constants
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+from constants import DB_PATH
+from model import Base, AuthModel, EntryModel
 
+engine = create_engine("sqlite:///"+DB_PATH, echo=True, future=True)
+Base.metadata.create_all(engine)
 
-engine = create_engine("sqlite://", echo=True, future=True)
+session = Session(engine)
 
+def get_auth_by_salt(salt):
+    res = session.query(AuthModel).filter(AuthModel.salt == salt).first()
+    return res
 
+def get_all_auth():
+    return session.query(AuthModel).all()
 
+# auth = AuthModel("test-name", "test-salt")
+# session.add(auth)
+# session.commit()
+# res = session.query(AuthModel).all()
+# print(res)
+
+print('LIST OF AUTH TOKENS: ', get_all_auth())
+
+res = get_auth_by_salt('test-salt')
+print('FOUND TOKEN: ', res)
